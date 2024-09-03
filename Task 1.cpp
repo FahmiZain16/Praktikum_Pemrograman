ce4be7aeb045b5f6682a6b3bb329e83f3427f8c4
FahmiZain16 <fahmiabdillahzain2006@mail.ugm.ac.id>
2024-09-04 06:08:48 +0700

    Add files via upload

--- Task 1.cpp
+++ Task 1.cpp
@@ -8,21 +8,28 @@ long long installment = 200000;
 long long insurance = 150000;
 
 int main(){
+	//Memberikan judul untuk slip gaji karyawan
 	cout << "Payslip for Employee\n";
 	cout << "----------------------" << endl;
+	
+	//memasukkan nama lengkap karyawan
 	cout << "Input your name : ";
 	getline(cin,name);
 	cout << endl;
+	
+	//memasukkan gaji bruto atau gaji kotor
 	cout << "Input your Gross Salary : ";
 	cin >> gross_salary;
 	cout << endl;
-	cout <<"Name \t\t: "<< name << endl;
-	cout <<"Gross Salary \t: Rp" << gross_salary << endl;
-	tax = gross_salary/5;
-	cout << "Tax (20%) \t: Rp" << tax << endl;
-	cout << "Installment \t: Rp"<<installment << endl;
-	cout << "Insurance \t: Rp" << insurance << endl;
-	net_salary = gross_salary - tax - installment - insurance;
-	cout << "Net Salary \t: Rp" << net_salary;
+	
+	//hasil keluaran adalah sebagai berikut : 
+	cout <<"Name \t\t: "<< name << endl; //mengeluarkan nama karyawan
+	cout <<"Gross Salary \t: Rp" << gross_salary << endl; //mengeluarkan gaji kotor karyawan
+	tax = gross_salary/5; // melakukan perhitungan pajak sebesar 20% dari gaji kotor
+	cout << "Tax (20%) \t: Rp" << tax << endl; //mengeluarkan pajak yang akan memotong gaji
+	cout << "Installment \t: Rp"<<installment << endl; //mengeluarkan biaya cicilan yang bersifat tetap
+	cout << "Insurance \t: Rp" << insurance << endl; //mengeluarkan biaya asuransi yang bersifat tetap
+	net_salary = gross_salary - tax - installment - insurance; // perhitungan gaji bersih => gaji kotor - pajak - cicilan - biaya asuransi
+	cout << "Net Salary \t: Rp" << net_salary; //mengeluarkan gaji bersih yang diterima karyawan
 	
 }
