2aeff18e717384bda38455219282aa656288d46f
FahmiZain16 <fahmiabdillahzain2006@mail.ugm.ac.id>
2024-09-04 19:06:52 +0700

    Add files via upload

--- test case task 1.cpp
+++ test case task 1.cpp
@@ -12,13 +12,13 @@ int main(){
 	cout << "Payslip for Employee\n";
 	cout << "--------------------------------" << endl;
 	
-	//hasil keluaran adalah sebagai berikut : 
+		//hasil keluaran adalah sebagai berikut : 
 	cout <<"Name \t\t: ";
 	getline(cin, name); //memasukkan nama karyawan
 	cout <<"Gross Salary \t: Rp";
 	cin >> gross_salary; "\n"; //memasukkan gaji kotor karyawan
-	tax = gross_salary/5; // melakukan perhitungan pajak sebesar 20% dari gaji kotor
-	cout << "Tax (20%) \t: Rp" << tax << endl; //mengeluarkan pajak yang akan memotong gaji
+	tax = gross_salary*15/100; //melakukan perhitungan pajak sebesar 20% dari gaji kotor
+	cout << "Tax (15%) \t: Rp" << tax << endl; //mengeluarkan pajak yang akan memotong gaji
 	cout << "Installment \t: Rp";
 	cin >> installment; //memasukkan biaya cicilan yang bersifat tetap
 	cout << "Insurance \t: Rp"; 
