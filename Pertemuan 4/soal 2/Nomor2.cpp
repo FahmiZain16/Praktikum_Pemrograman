b2132262e4a2000fa046e57b21696e6123df3621
FAHMI ABDILLAH ZAIN <fahmiabdillahzain2006@mail.ugm.ac.id>
2024-09-18 20:58:24 +0700

    Add files via upload

--- Pertemuan 4/soal 2/Nomor2.cpp
+++ Pertemuan 4/soal 2/Nomor2.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 int main(){
  int angka, hasil;
+ cout << "Input : ";
  cin >> angka;
+ cout << "Output : "<< endl;
  for(int i = 1; i <= 10; i++){
  	hasil = angka*i;
  	cout << angka << " x " << i <<" = "<< hasil << endl;
