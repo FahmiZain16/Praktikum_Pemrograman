378f027c859591976cbf6ff1cf776e7a7459ce63
FAHMI ABDILLAH ZAIN <fahmiabdillahzain2006@mail.ugm.ac.id>
2024-09-18 20:55:14 +0700

    Add files via upload

--- Pertemuan 3/Nomor 2/Nomor2.cpp
+++ Pertemuan 3/Nomor 2/Nomor2.cpp
@@ -2,25 +2,13 @@
 using namespace std;
 
 int main(){
-	cout << "--Program Mencari Bilangan Terbesar dari Tiga Angka--\n\n\n";
-	
-	float a,b,c;
-	cout << "Inputkan bilangan 1 : ";
-	cin >> a;
-	cout << "Inputkan bilangan 2 : ";
-	cin >> b;
-	cout << "Inputkan bilangan 3 : ";
-	cin >> c;
-	if(a > b){
-		if(a > c) cout << "Bilangan terbesar adalah : "<< a;
-		else {
-			cout << "Bilangan terbesar adalah : "<< c;
-		}
-	} else {
-		if(b > c) cout << "Bilangan terbesar adalah : "<< b;
-		else {
-			cout << "Bilangan terbesar adalah : "<< c;
-		}
-	}
-
+ int angka, hasil;
+ cout << "Input : ";
+ cin >> angka;
+ cout << "Output : "<< endl;
+ for(int i = 1; i <= 10; i++){
+ 	hasil = angka*i;
+ 	cout << angka << " x " << i <<" = "<< hasil << endl;
+ }
+ 
 }
